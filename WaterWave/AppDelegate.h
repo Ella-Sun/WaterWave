0d03496012a3cf6047266cd2881ca20ae5e14260
sunhong <sunhong@ucfgroup.com>
2016-06-21 09:41:46 +0800

    Initial Commit

---
+++ WaterWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterWave
+//
+//  Created by sunhong on 16/6/21.
+//  Copyright © 2016年 Sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaterWave/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WaterWave
+//
+//  Created by sunhong on 16/6/21.
+//  Copyright © 2016年 Sunhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
